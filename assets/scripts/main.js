ca8c340c85aedf54b69505a52456779075ffc7a0
bpanuco11 <146768736+bpanuco11@users.noreply.github.com>
2024-05-17 20:45:11 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -69,23 +69,53 @@ async function getRecipes() {
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   /**************************/
+  const storedRecipes = localStorage.getItem('recipes');
+  if (storedRecipes) {
+    return JSON.parse(storedRecipes);
+    //return JSON.parse(storedRecipes) || [];
+  }
+
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let recipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
-  // A4-A11 will all be *inside* the callback function we passed to the Promise
-  // we're returning
-  /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
+  return new Promise(async (resolve, reject) => {
+    // A4-A11 will all be *inside* the callback function we passed to the Promise
+    // we're returning
+    /**************************/
+    // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+    //            declared above
+    for (let url of RECIPE_URLS) {
+      // A5. TODO - Since we are going to be dealing with asynchronous code, create
+      //            a try / catch block. A6-A9 will be in the try portion, A10-A11
+      //            will be in the catch portion.
+      try { 
+        // A6
+        const response = await fetch(url);
+        // A7. 
+        const recipe = await response.json();
+        // A8. 
+        recipes.push(recipe);
+        // A9. 
+        if (recipes.length === RECIPE_URLS.length) {
+          saveRecipesToStorage(recipes);
+          resolve(recipes);
+        }
+      } catch (err) {
+        // A10. 
+        console.error(err);
+        // A11. 
+        reject(err);
+      }
+    }// end loop     
+  });//end inline function()
+
   // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
   //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
   //            you must either use "await fetch(...)" or "fetch.then(...)". This
@@ -100,7 +130,8 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
-}
+
+}// end getRecipes()
 
 /**
  * Takes in an array of recipes, converts it to a string, and then
