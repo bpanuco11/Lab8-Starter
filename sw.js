b92f46dcaacf96b7f8b1ad8b9cf5af24e7faf985
bpanuco11 <brandonpanuco2270@gmail.com>
2024-05-18 01:44:08 -0700

    updated Explore section

--- sw.js
+++ sw.js
@@ -2,6 +2,14 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-8-starter';
+const RECIPE_URLS = [
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+];
 
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
@@ -9,7 +17,9 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      //const urls = RECIPE_URLS.map(obj => obj.url);
+      //return cache.addAll([urls]);
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -32,9 +42,63 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
+  // Respond to the event by opening the cache using the name we gave above (CACHE_NAME)
+  
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        if (response) {
+          console.log('Found in cache:', event.request.url);
+          return response;
+        }
+
+        return fetch(event.request).then(function (networkResponse) {
+          // Put the network response directly into the cache
+          cache.put(event.request, networkResponse);
+          return networkResponse;
+        });
+      });
+    })
+  ); 
+  
 });
+
+/*event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        if (response) {
+          console.log('Found in cache:', event.request.url);
+          return response;
+        }
+
+        return fetch(event.request).then(function (networkResponse) {
+          // Put the network response directly into the cache
+          cache.put(event.request, networkResponse);
+          return networkResponse;
+        });
+      });
+    })
+  ); 
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        // If the request is in the cache, return the cached version
+        if (response) {
+          return response;
+        }
+
+        // Otherwise fetch the resource, add it to the cache, and return network response
+        return fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+  */
